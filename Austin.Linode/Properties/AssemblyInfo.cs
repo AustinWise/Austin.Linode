b94a0193c79a2d492daf371801afbf26f6a78a8f
Austin Wise <AustinWise@gmail.com>
2017-01-01 19:39:26 -0800

    Version bump.

--- Austin.Linode/Properties/AssemblyInfo.cs
+++ Austin.Linode/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Austin.Linode")]
@@ -7,12 +6,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Austin Wise")]
 [assembly: AssemblyProduct("Austin.Linode")]
-[assembly: AssemblyCopyright("Copyright © Austin Wise 2016")]
+[assembly: AssemblyCopyright("Copyright © Austin Wise 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("d012d95d-8bbe-4382-b610-8cd1205fbd36")]
 
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
